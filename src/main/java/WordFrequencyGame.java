4772c53e824ed3bf0d0e46bf568fb796328d874c
Kelvin Lee <kelvin.lee@oocl.com>
2020-04-14 16:21:37 +0800

    [WordFrequencyGame] restrict access modifier public -> private

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -5,18 +5,18 @@ public class WordFrequencyGame {
     private final String SEPARATOR_PATTERN = "\\s+";
     private final String LINE_BREAK = "\n";
 
-    public List<String> splitInputString(String inputString){
+    private List<String> splitInputString(String inputString){
         return Arrays.asList(inputString.split(SEPARATOR_PATTERN));
     }
 
-    public List<WordInfo> sortByWordFrequency(List<WordInfo> wordCount){
+    private List<WordInfo> sortByWordFrequency(List<WordInfo> wordCount){
         return wordCount
                 .stream()
                 .sorted(Comparator.comparingInt(WordInfo::getCount).reversed())
                 .collect(Collectors.toList());
     }
 
-    public List<WordInfo> countWordFrequency(List<String> inputStringArray){
+    private List<WordInfo> countWordFrequency(List<String> inputStringArray){
         List<String> uniqueInputString = new ArrayList<String>(new HashSet<String>(inputStringArray));
         return uniqueInputString
                 .stream()
